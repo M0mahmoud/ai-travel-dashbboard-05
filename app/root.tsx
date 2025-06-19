6161bd513a21bfb2e5fabece6942163ded95f627
Mahmoud Mohamed <88073988+M0mahmoud@users.noreply.github.com>
2025-06-19 21:40:00 +0300

    - Add suppressHydrationWarning to body - Implement HydrateFallback component

--- app/root.tsx
+++ app/root.tsx
@@ -72,12 +72,24 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
+  );
+}
+
+export function HydrateFallback() {
+  return (
+    <div className="h-dvh w-dvw flex justify-center items-center">
+      <div className="flex-col gap-4 w-full flex items-center justify-center">
+        <div className="w-20 h-20 border-4 border-transparent text-primary-500 text-4xl animate-spin flex items-center justify-center border-t-primary-500 rounded-full">
+          <div className="w-16 h-16 border-4 border-transparent text-dark-300 text-2xl animate-spin flex items-center justify-center border-t-dark-300 rounded-full" />
+        </div>
+      </div>
+    </div>
   );
 }
 
